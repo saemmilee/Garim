19d19f0e0e44e05bb31a71509d424c2c0d2724ef
saemmilee <saemmi.lee@gmail.com>
2021-07-26 23:25:54 +0900

    first commit

---
+++ app/src/main/java/com/inhatc/garim/MainActivity.java
@@ -0,0 +1,24 @@
+package com.inhatc.garim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // sm
+        // firebaseDatabase Test
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        databaseReference.child("message").push().setValue("2");
+
+    }
+}
