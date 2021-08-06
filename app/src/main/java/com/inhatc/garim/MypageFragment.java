b3aa33501c1ebcc5e1c92f7d8ce29a19463a6436
tjshee39 <gksh054@naver.com>
2021-08-06 13:29:18 +0900

    20210729:: 1sunny

--- app/src/main/java/com/inhatc/garim/MypageFragment.java
+++ app/src/main/java/com/inhatc/garim/MypageFragment.java
@@ -1,5 +1,6 @@
 package com.inhatc.garim;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +8,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MypageFragment extends Fragment {
 
-//    public MypageFragment() {
-//        // Required empty public constructor
-//    }
+    Button btnSignature;
+    Button btnStatus;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_ordinancestatus, container, false);
-        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        btnSignature = (Button) v.findViewById(R.id.btnSignature);
+        btnStatus = (Button) v.findViewById(R.id.btnStatus);
+
+        btnSignature.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ElectronicSignatureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStatus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ApplicationStatusActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
 }
