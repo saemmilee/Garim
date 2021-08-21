f212fcbb916d20807a504330a6897ef6c9c9aee8
saemmilee <saemmi.lee@gmail.com>
2021-08-21 22:57:14 +0900

    0821_merge jh

--- app/src/main/java/com/inhatc/garim/ApplyFragment.java
+++ app/src/main/java/com/inhatc/garim/ApplyFragment.java
@@ -19,7 +19,6 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.HashMap;
 
